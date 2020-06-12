76eb77d5edf08ee28f825b8c63eac902d365aae2
Vincent Robles <sylkisgsx@gmail.com>
2020-06-12 02:52:55 -0400

    starts new game

---
+++ src/components/NewGameButton/NewGameButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewGameButton = (props) => (
+  <button className='btn btn-default' onClick={props.handleNewGameClick}>
+    New Game
+  </button>
+);
+
+export default NewGameButton;
