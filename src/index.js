14f63df70086def1435a80d21b1bb3be149991eb
Vincent Robles <sylkisgsx@gmail.com>
2020-06-12 02:45:37 -0400

    router wraps around app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './pages/App/App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+    <Route component={App} />
+  </Router>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
